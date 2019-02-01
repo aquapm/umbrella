cbd931598a3304847c2c0f072c7712f85d1654e2
Dmitry Rubinstein <rubinsteindb@gmail.com>
2019-02-01 13:09:03 +0200

    Fix tests generation

--- template/config/config.exs
+++ template/config/config.exs
@@ -3,3 +3,8 @@ use Mix.Config
 import_config "../apps/*/config/config.exs"
 
 # Umbrella configuration (overrides the imported configuration above):
+<%= if @test do %>
+# Import environment specific config. This must remain at the bottom
+# of this file so it overrides the configuration defined above.
+import_config "#{Mix.env()}.exs"
+<% end %>
