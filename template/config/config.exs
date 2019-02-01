aa1cf7c4af3bf35d7428df58051948233112f739
Dmitry Rubinstein <rubinsteindb@gmail.com>
2019-02-01 13:18:43 +0200

    Fix newline

--- template/config/config.exs
+++ template/config/config.exs
@@ -6,5 +6,4 @@ import_config "../apps/*/config/config.exs"
 <%= if @test do %>
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
-import_config "#{Mix.env()}.exs"
-<% end %>
+import_config "#{Mix.env()}.exs"<% end %>
--- template/mix.exs
+++ template/mix.exs
@@ -9,11 +9,6 @@ defmodule <%= @project_name_camel_case %>.MixProject do
     ]
   end
 
-  # Dependencies listed here are available only for this
-  # project and cannot be accessed from applications inside
-  # the apps folder.
-  #
-  # Run "mix help deps" for examples and options.
   defp deps do
     []
   end
